439e0b34b8764d491e7ab7f1456c417de26a9f43
Michael Modon <michael.modon@gmail.com>
2015-01-31 12:16:12 -0500

    Add Pins and users association.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,44 +1,56 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   respond_to :html
 
   def index
     @pins = Pin.all
-    respond_with(@pins)
+   # respond_with(@pins)
   end
 
   def show
-    respond_with(@pin)
+   # respond_with(@pin)
   end
 
   def new
-    @pin = Pin.new
-    respond_with(@pin)
+    @pin = current_user.pins.build
+    #respond_with(@pin)
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
-    @pin.save
-    respond_with(@pin)
+    @pin = current_user.pins.build(pin_params)
+if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+  end
+end
+
+def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
-  def update
-    @pin.update(pin_params)
-    respond_with(@pin)
-  end
 
   def destroy
     @pin.destroy
-    respond_with(@pin)
+  #  respond_with(@pin)
   end
 
   private
     def set_pin
       @pin = Pin.find(params[:id])
+    end
+def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     def pin_params
